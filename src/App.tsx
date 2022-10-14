516e8f37952f2e47c45ddedaeaa47be2d34b5bc5
Dimitry-prog <kfifa3@gmail.com>
2022-10-14 16:51:38 +0300

    feat: Videos.jsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
 import { Performances } from './components/Performances/Performances';
+import Videos from "./components/Videos/Videos";
 
 function App() {
   return (
     <main>
       <h1>Lubimovka</h1>
       <Performances />
+        <Videos/>
     </main>
   );
 }
---
+++ src/components/Video/Video.tsx
@@ -0,0 +1,20 @@
+import React, {FC} from 'react';
+import styles from './Video.module.scss';
+
+interface VideoProps {
+    src: string
+}
+
+const Video: FC<VideoProps> = ({src}) => {
+    return (
+        <iframe
+            src={src}
+            title="Youtube Player"
+            allowFullScreen
+            className={styles.video}
+        />
+    );
+};
+
+export default Video;
+
