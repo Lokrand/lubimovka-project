52edcafdbd2c812a2e067900facb7358a1af5798
Dima <Uchiha132132qqq@gmail.com>
2022-10-19 20:18:36 +0300

    стили для спектаклей на мобилках

--- src/components/Performance/Performance.jsx
+++ src/components/Performance/Performance.jsx
@@ -2,23 +2,26 @@ import styles from "./Performance.module.scss";
 
 export const Performance = ({ props }) => {
   const blackArrow = require("../../images/Arrow_for_button.svg").default;
-  
+
 
   return (
-    <div className={styles.block}>
-      <img
-        className={styles.image}
-        src="https://img.desktopwallpapers.ru/rocks/pics/wide/1920x1200/27640f370156a0e0ae3ee9608fc8480a.jpg"
-        alt=""
-      />
-      <div className={styles.description}>
-        <div className={styles.data_block}>
-          <p>{props.data}</p>
-          <p>{props.time}</p>
-        </div>
-        <h3 className={styles.title}>{props.title}</h3>
-        <p className={styles.directors}>{props.directors}</p>
-        <p className={styles.read}>читка проекта Любимовка.Ещё</p>
+    <div>
+      <div className={styles.block}>
+        <img
+          className={styles.image}
+          src="https://img.desktopwallpapers.ru/rocks/pics/wide/1920x1200/27640f370156a0e0ae3ee9608fc8480a.jpg"
+          alt=""
+        />
+        <div className={styles.description}>
+          <div className={styles.data_block}>
+            <p>{props.data}</p>
+            <p>{props.time}</p>
+          </div>
+          <h3 className={styles.title}>{props.title}</h3>
+          <p className={styles.directors}>{props.directors}</p>
+          <p className={styles.read}>читка проекта Любимовка.Ещё</p>
+          </div>
+      </div>
         <div className={styles.buttons}>
           <button className={styles.button}>
             <img
@@ -36,7 +39,6 @@ export const Performance = ({ props }) => {
             />
             БИЛЕТЫ
           </button>
-        </div>
       </div>
     </div>
   );
