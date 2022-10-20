23c3df6604a157740de3ac8f6d3a5cce6271d021
Dimitry-prog <kfifa3@gmail.com>
2022-10-20 22:01:01 +0300

    feat: Gallery

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,15 @@ import logo from './logo.svg';
 import './App.scss';
 import { Performances } from './components/Performances/Performances';
 import Videos from "./components/Videos/Videos";
+import Gallery from "./components/Gallery/Gallery";
 
 function App() {
   return (
     <main>
       <h1>Lubimovka</h1>
       <Performances />
-        <Videos/>
+      <Videos/>
+      <Gallery/>
     </main>
   );
 }
