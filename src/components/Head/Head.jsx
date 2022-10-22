ed3efd4fed3672130929e804197fa6bf46417e34
burlackoff <burlackoff@gmail.com>
2022-10-22 19:02:08 +0300

    feat: add Head component

--- src/components/Head/Head.jsx
+++ src/components/Head/Head.jsx
@@ -1,5 +1,21 @@
+import styles from "./Head.module.scss";
+import audience from "../../images/audience.jpg";
+
 function Head() {
-  return <section></section>;
+  return (
+    <section className={styles.section}>
+      <h1 className={styles.title}>Любимовка. Ещё</h1>
+      <p className={styles.subtitle}>
+        Межсезонные читки и обсуждение пьес из списка отмеченных отборщиками
+        Любимовки.
+      </p>
+      <img
+        src={audience}
+        alt="Читка пьес в большой аудитории."
+        className={styles.image}
+      />
+    </section>
+  );
 }
 
 export default Head;
