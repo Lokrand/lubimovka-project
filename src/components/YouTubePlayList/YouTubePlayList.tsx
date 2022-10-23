aa96d1257c3faa4cb07cb446cb4a466740aed879
Dimitry-prog <kfifa3@gmail.com>
2022-10-23 21:25:01 +0300

    fix: moved svg to global scope

--- src/components/YouTubePlayList/YouTubePlayList.tsx
+++ src/components/YouTubePlayList/YouTubePlayList.tsx
@@ -10,7 +10,7 @@ const YouTubePlayList = () => {
                 <p>Видео прошедших мероприятий можно посмотреть в плейлисте на канале Любимовки в YouTube</p>
                 <a href={'#'} target="_blank" rel="noreferrer noopener">
                     YOUTUBE
-                    <img src={arrowRight} alt='стрелка'/>
+                    <img src={arrowRight} alt='стрелка вправо'/>
                 </a>
             </div>
 
