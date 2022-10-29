a917d7878e86ab4de139367a72b1ff0859e9fe54
Dima <Uchiha132132qqq@gmail.com>
2022-10-29 21:52:42 +0300

    some fix

--- src/components/CooperationProject/CooperationProject.tsx
+++ src/components/CooperationProject/CooperationProject.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './CooperationProject.module.scss';
-import snowflake from './images/asterisk.svg';
+import snowflake from '../../images/cooperationProject/asterisk.svg';
 
 const CooperationProject = () => {
     return (
