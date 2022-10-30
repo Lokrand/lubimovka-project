df94b7b1cec8b7d2489d173ff32177a80cfee378
Dima <Uchiha132132qqq@gmail.com>
2022-10-30 12:59:29 +0300

    remove some imports

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,22 +4,12 @@ import Videos from "../Videos/Videos";
 import Header from "../Header/Header";
 import { Persons } from "../Persons/Persons";
 import { Pieces } from "../Pieces/Pieces";
-<<<<<<< HEAD
-<<<<<<< HEAD
 import YouTubePlayList from "../YouTubePlayList/YouTubePlayList";
-=======
-import Head from "../Head/Head";
 import Gallery from "../Gallery/Gallery";
 import About from "../About/About";
->>>>>>> 7bb8088d0900f86b4fc482977e1bcefc688ff045
-=======
-import YouTubePlayList from "../YouTubePlayList/YouTubePlayList";
 import Footer from "../Footer/Footer";
 import CooperationProject from "../CooperationProject/CooperationProject";
 import Description from "../Description/Description";
-import Gallery from "../Gallery/Gallery";
-import About from "../About/About";
->>>>>>> 8cc1a408ada1066d62f40b76fd358dad8e776415
 
 function App() {
   return (
@@ -33,16 +23,10 @@ function App() {
         <Performances />
         <Pieces />
         <Persons />
-<<<<<<< HEAD
-<<<<<<< HEAD
         <YouTubePlayList/>
-=======
         <Gallery />
->>>>>>> 7bb8088d0900f86b4fc482977e1bcefc688ff045
-=======
         <YouTubePlayList />
         <CooperationProject />
->>>>>>> 8cc1a408ada1066d62f40b76fd358dad8e776415
       </main>
       <Footer />
     </>
